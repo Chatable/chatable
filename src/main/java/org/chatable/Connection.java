400e9a434b1b4119c2201c22314b89f2c72b6387
Jack Gerrits <jackgerrits95@gmail.com>
2015-02-02 17:49:31 +1030

    basic connection constructor

--- src/main/java/org/chatable/Connection.java
+++ src/main/java/org/chatable/Connection.java
@@ -1,8 +1,10 @@
 package org.chatable;
 
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -14,21 +16,21 @@ public class Connection {
     private String ip;
     private int port;
     private Socket socket;
-    private static final Logger logger = LogManager.getLogger();
+    private static final Logger logger = LogManager.getLogger(Connection.class);
 
     public Connection(String ip, int port) {
         this.ip = ip;
         this.port = port;
 
         try{
-
-
+            socket = new Socket();
+            socket.connect(new InetSocketAddress(ip,port),4000);
         } catch (UnknownHostException e) {
-
+            logger.error(e.toString());
         } catch (IOException e) {
-
+            logger.error(e.toString());
         } catch (Exception e) {
-
+            logger.error(e.toString());
         }
 
     }
