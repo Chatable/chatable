92e90861b3b90bbec4d63ed3b0729135d4725159
henrystevens <henrystevens@gmail.com>
2015-02-02 17:18:43 +1030

    Added Log4j2.xml

--- src/main/java/org/chatable/Main.java
+++ src/main/java/org/chatable/Main.java
@@ -1,9 +1,14 @@
 package org.chatable;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Created by henrystevens on 2/02/15.
  */
 public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
 
     public Main() {
         System.out.println("this is a big test");
