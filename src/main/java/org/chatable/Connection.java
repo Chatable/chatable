f7a89acb8b4fcaac4bdfa2fe2f64419d567f03b7
Jack Gerrits <jackgerrits95@gmail.com>
2015-02-02 18:09:12 +1030

    send/receive functionality to connection

--- src/main/java/org/chatable/Connection.java
+++ src/main/java/org/chatable/Connection.java
@@ -3,7 +3,10 @@ package org.chatable;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -16,6 +19,8 @@ public class Connection {
     private String ip;
     private int port;
     private Socket socket;
+    private PrintWriter output;
+    private BufferedReader input;
     private static final Logger logger = LogManager.getLogger(Connection.class);
 
     public Connection(String ip, int port) {
@@ -32,6 +37,65 @@ public class Connection {
         } catch (Exception e) {
             logger.error(e.toString());
         }
+    }
 
+    public Connection(Socket socket) {
+        try{
+            this.socket = socket;
+            output = new PrintWriter(socket.getOutputStream(), true);
+            input = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
+            ip = socket.getRemoteSocketAddress().toString();
+            port = socket.getPort();
+        } catch (UnknownHostException e) {
+            logger.error(e.toString());
+        } catch (IOException e) {
+            logger.error(e.toString());
+        } catch (Exception e) {
+            logger.error(e.toString());
+        }
+    }
+
+    public boolean send(String input){
+        if(!isConnected()){
+            return false;
+        }
+
+        output.println(input);
+
+        if(output.checkError()){
+            output.flush();
+            return false;
+        }
+        return true;
+    }
+
+    public String read(){
+        if(!isConnected()){
+            return null;
+        }
+
+        String message = null;
+        try {
+            socket.setSoTimeout(1000);
+            message = input.readLine();
+        } catch (IOException e) {
+            logger.error(e.toString());
+            return null;
+        }
+
+        return message;
+    }
+
+    public boolean isConnected(){
+        return(socket.isConnected());
+    }
+
+    public String getIP(){
+        return ip;
+    }
+
+    public int getPort(){
+        return port;
     }
 }
