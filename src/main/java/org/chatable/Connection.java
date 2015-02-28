0ea370a36f78256aa91f7adc0a228c11fb064465
Jack Gerrits <jackgerrits95@gmail.com>
2015-02-28 11:09:40 +1030

    handle sending data from server to client websocket

--- src/main/java/org/chatable/Connection.java
+++ src/main/java/org/chatable/Connection.java
@@ -3,10 +3,7 @@ package org.chatable;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.security.MessageDigest;
@@ -26,6 +23,8 @@ public class Connection {
     private Socket socket;
     private InputListener listener;
     private Server server;
+    private OutputStream os;
+    private InputStream is;
     private PrintWriter output;
     private BufferedReader input;
     private static final Logger logger = LogManager.getLogger(Connection.class);
@@ -46,9 +45,10 @@ public class Connection {
         try{
             this.socket = socket;
             this.server = server;
-            output = new PrintWriter(socket.getOutputStream(), true);
-            input = new BufferedReader(new InputStreamReader(
-                    socket.getInputStream()));
+            os = socket.getOutputStream();
+            output = new PrintWriter(os, true);
+            is = socket.getInputStream();
+            input = new BufferedReader(new InputStreamReader(is));
             ip = socket.getRemoteSocketAddress().toString();
             port = socket.getPort();
         } catch (Exception e) {
@@ -115,14 +115,68 @@ public class Connection {
             return false;
         }
 
-        output.println(input);
-        //output.println("name: " + input);
+        if(sockType == SocketType.SOCKET){
+            output.println(input);
 
-        if(output.checkError()){
-            output.flush();
+            if(output.checkError()){
+                output.flush();
+                return false;
+            }
+            return true;
+        } else if (sockType == SocketType.WEBSOCKET) {
+            byte[] rawData = input.getBytes();
+            int frameCount = 0;
+            byte[] frame = new byte[10];
+            frame[0] = (byte) 129;
+
+            if (rawData.length <= 125) {
+                frame[1] = (byte) rawData.length;
+                frameCount = 2;
+            } else if (rawData.length >= 126 && rawData.length <= 65535) {
+                frame[1] = (byte) 126;
+                int len = rawData.length;
+                frame[2] = (byte) ((len >> 8));
+                frame[3] = (byte) (len);
+                frameCount = 4;
+            } else {
+                frame[1] = (byte) 127;
+                int len = rawData.length;
+                frame[2] = (byte) ((len >> 24));
+                frame[3] = (byte) ((len >> 16));
+                frame[4] = (byte) ((len >> 8));
+                frame[5] = (byte) ((len));
+                frame[6] = (byte) ((len >> 24));
+                frame[7] = (byte) ((len >> 16));
+                frame[8] = (byte) ((len >> 8));
+                frame[9] = (byte) (len);
+                frameCount = 10;
+            }
+
+            int bLength = frameCount + rawData.length;
+
+            byte[] reply = new byte[bLength];
+
+            int bLim = 0;
+            for (int i = 0; i < frameCount; i++) {
+                reply[bLim] = frame[i];
+                bLim++;
+            }
+            for (byte aRawData : rawData) {
+                reply[bLim] = aRawData;
+                bLim++;
+            }
+
+            try {
+                os.write(reply);
+                os.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+            return true;
+        } else {
             return false;
         }
-        return true;
     }
 
     /**
@@ -134,17 +188,23 @@ public class Connection {
             return null;
         }
 
-        String message = null;
-        try {
-            socket.setSoTimeout(1000);
-            message = input.readLine();
-        } catch (SocketTimeoutException e) {
-            //Standard read time out. Not bad at all.
-        } catch (IOException e) {
-            logger.error(e.toString());
+        if(sockType == SocketType.SOCKET){
+            String message = null;
+            try {
+                socket.setSoTimeout(1000);
+                message = input.readLine();
+            } catch (SocketTimeoutException e) {
+                //Standard read time out. Not bad at all.
+            } catch (IOException e) {
+                logger.error(e.toString());
+            }
+
+            return message;
+        } else if (sockType == SocketType.WEBSOCKET){
+
         }
 
-        return message;
+        return null;
     }
 
     /**
@@ -155,6 +215,8 @@ public class Connection {
         input.close();
         output.flush();
         output.close();
+        os.flush();
+        os.close();
         socket.close();
         listener.stop();
         System.out.println("Connection closed.");
